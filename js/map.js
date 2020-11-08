7b814635c880670ab342ae95f17c19d2c7bfe93e
kelter-js <spawnkillerqw@gmail.com>
2020-11-08 10:17:05 +0400

    Поправил файлы

--- js/map.js
+++ js/map.js
@@ -2,17 +2,3 @@ const map = document.querySelector('.map-button');
 const modalMap = document.querySelector('.modal-map');
 const mapImage = document.querySelector('.map-button img');
 
-map.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  modalMap.classList.toggle('make-visible');
-})
-document.addEventListener('click', function (evt) {
-  let target = evt.target;
-  let currentElemContactUs = target == modalMap || modalMap.contains(target);
-  let isntButtonContact = target == map;
-  let isntButtonImg = target == mapImage;
-  let modalIsActiveContact = modalMap.classList.contains('make-visible');
-  if(!currentElemContactUs && modalIsActiveContact && !isntButtonContact && !isntButtonImg) {
-    modalMap.classList.toggle('make-visible');
-  };
-})
